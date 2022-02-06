2331f5bf3275a74db69da27f410836763d877231
Jimothy <phazonjim@gmail.com>
2022-02-06 12:49:08 -0500

    Update README, add test directory, update dependencies

--- README.md
+++ README.md
@@ -2,20 +2,44 @@
 A set of tools that allows the use of python plugins to easily process comment, submission, and modlog streams from PRAW (Python reddit API wrapper)
 
 # Environment setup instructions
-1. Install Python 3
-2. Install PIP (https://pip.pypa.io/en/stable/installing/)
-3. pip install -r requirements.txt
+1. Ensure Python 3.6 or higher is installed
+2. (Optional) to setup a local virtual environment you can run `python -m venv .venv`
+3. run `pip install -r requirements.txt` to install dependencies
 
-# Configurations
+# Configuration
 1. Rename config.example to config.yaml
-2. Ensure user info, subreddit name, etc are set up in config.yaml
-3. Any plugins must be dropped into the plugins folder
+    - Set `client` fields
+    - Set `subreddit` name
+    - Toggle which reddit streams you need access to under the `streams` field
+        - Currently, the options are `comments`, `submissions`, `modlog`, and `modmail`.
+    - Configure which plugins you want activated under the `plugins` field
+2. All plugins must be placed into the plugins directory located at `/reddit-automation-suite/plugins/`
+    - When toggling which plugins to run in config.yaml, the program finds the plugin by knowing which directory to look in, the filename of the main class, and the name of the main class to instantiate. 
+    - Example plugin directory structure:
+        ```bash
+        plugins
+        ├── VoteBot
+        │   ├── main.py
+        |   |   └──class Foo
+        │   └── config.yaml
+        └── Events
+            ├── Baz.py
+            |   └──class Bar
+            ├── log.txt
+            └── config.yaml
+        ```
+        The main config.yaml `plugins` section would look like this:
+        ```yaml
+        plugins:
+        - "VoteBot.main.Foo"
+        - "Events.Baz.Bar"
+        ```
+3. All loaded plugin classes are expected to have class methods of `consume_comment`, `consume_submission`, `consume_mod_log`, and `consume_modmail`. A `PluginBase` class can be found in `utils.py` and inherited from which has the aforementioned methods set as well as some other useful methods for loading config files and caching data.
 
 # Usage
-1. run main.py
+1. `cd` into project workspace
+2. run `python -m reddit-automation-suite`
 
-# Features
-1. TBD
-
-# TODO
-1. TBD
+# Testing
+1. `cd` into project workspace
+2. run `python -m pytest tests/ --cov=lower`
---
+++ reddit-automation-suite/loader.py
@@ -0,0 +1,97 @@
+import os
+import logging
+from importlib import import_module
+import praw
+from .utils import PluginBase
+
+
+class RedditAssitant(PluginBase):
+    def __init__(self):
+        PluginBase.__init__(
+            self,
+            name="__base",
+            config_path=os.path.join(os.path.dirname(__file__), "config.yaml"),
+        )
+        self.init_cache()
+        self.reddit = self.init_reddit()
+        self.subreddit = self.reddit.subreddit(self.config["subreddit"])
+        self.plugins = self.load_plugins()
+
+    def init_reddit(self):
+        client = self.config["client"]
+        reddit = praw.Reddit(**client)
+        return reddit
+
+    def init_cache(self):
+        cache_dir = os.path.join(os.path.dirname(__file__), "__cache")
+        if not os.path.isdir(cache_dir):
+            os.mkdir(cache_dir)
+
+    def load_plugins(self):
+        plugins = []
+        for plugin in self.config["plugins"]:
+            logging.info(f"Loading {plugin}")
+            dir_name, file_name, class_name = plugin.split(".")
+            cls = getattr(
+                import_module(f".plugins.{dir_name}.{file_name}", package=__package__),
+                class_name,
+            )
+            plugins.append(cls(self.reddit, self.subreddit))
+        return plugins
+
+    def stream_subreddit(self):
+        comment_stream = (
+            self.subreddit.stream.comments(skip_existing=True, pause_after=-1)
+            if self.config["streams"]["comments"]
+            else []
+        )
+        submission_stream = (
+            self.subreddit.stream.submissions(skip_existing=True, pause_after=-1)
+            if self.config["streams"]["submissions"]
+            else []
+        )
+        mod_stream = (
+            self.subreddit.mod.stream.log(skip_existing=True, pause_after=-1)
+            if self.config["streams"]["modlog"]
+            else []
+        )
+        modmail_stream = (
+            self.subreddit.mod.stream.modmail_conversations(
+                skip_existing=True, pause_after=-1
+            )
+            if self.config["streams"]["modmail"]
+            else []
+        )
+        while True:
+            for comment in comment_stream:
+                if comment is None:
+                    break
+                self.process_comment(comment)
+            for submission in submission_stream:
+                if submission is None:
+                    break
+                self.process_submission(submission)
+            for mod_log in mod_stream:
+                if mod_log is None:
+                    break
+                self.process_mod_log(mod_log)
+            for modmail in modmail_stream:
+                if modmail is None:
+                    break
+                self.process_modmail(modmail)
+
+    def process_comment(self, comment):
+        for plugin in self.plugins:
+            plugin.consume_comment(comment)
+
+    def process_submission(self, submission):
+        for plugin in self.plugins:
+            plugin.consume_submission(submission)
+
+    def process_mod_log(self, mod_log):
+        for plugin in self.plugins:
+            plugin.consume_mod_log(mod_log)
+
+    def process_modmail(self, modmail):
+        for plugin in self.plugins:
+            plugin.consume_modmail(modmail)
